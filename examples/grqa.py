d87b0f68cd0a2b3bde4071adb81926adbbd2f665
Ather Abbas <ather_abbas786@yahoo.com>
2022-12-28 09:37:16 +0900

    reduced grqa example

--- examples/grqa.py
+++ examples/grqa.py
@@ -33,99 +33,99 @@ print(params)
 
 # %%
 
-df = ds.fetch_parameter("TEMP", country=country)
-print(df.shape)
-for grp_name, grp in df.groupby('site_name'):
-    plot(grp['obs_value'], '--.', label=grp_name, show=False,
-         ax_kws=dict(figsize=(18,7)))
-plt.show()
-
-# %%
-df = ds.fetch_parameter("NH4N", country=country)
-print(df.shape)
-for grp_name, grp in df.groupby('site_name'):
-    plot(grp['obs_value'], '--.', label=grp_name, show=False,
-         ax_kws=dict(figsize=(18,7)))
-plt.show()
-
-# %%
-df = ds.fetch_parameter("DO", country=country)
-print(df.shape)
-for grp_name, grp in df.groupby('site_name'):
-    plot(grp['obs_value'], '--.', label=grp_name, show=False,
-         ax_kws=dict(figsize=(18,7)))
-plt.show()
-
-# %%
-df = ds.fetch_parameter("COD", country=country)
-print(df.shape)
-for grp_name, grp in df.groupby('site_name'):
-    plot(grp['obs_value'], '--.', label=grp_name, show=False,
-         ax_kws=dict(figsize=(18,7)))
-plt.show()
-
-# %%
-df = ds.fetch_parameter("BOD", country=country)
-print(df.shape)
-for grp_name, grp in df.groupby('site_name'):
-    plot(grp['obs_value'], '--.', label=grp_name, show=False,
-         ax_kws=dict(figsize=(18,7)))
-plt.show()
-
-# %%
-df = ds.fetch_parameter("DON", country=country)
-print(df.shape)
-for grp_name, grp in df.groupby('site_name'):
-    plot(grp['obs_value'], '--.', label=grp_name, show=False,
-         ax_kws=dict(figsize=(18,7)))
-plt.show()
-
-# %%
-df = ds.fetch_parameter("DOSAT", country=country)
-print(df.shape)
-for grp_name, grp in df.groupby('site_name'):
-    plot(grp['obs_value'], '--.', label=grp_name, show=False,
-         ax_kws=dict(figsize=(18,7)))
-plt.show()
-
-# %%
-df = ds.fetch_parameter("TDP", country=country)
-print(df.shape)
-for grp_name, grp in df.groupby('site_name'):
-    plot(grp['obs_value'], '--.', label=grp_name, show=False,
-         ax_kws=dict(figsize=(18,7)))
-plt.show()
-
-# %%
-df = ds.fetch_parameter("TKN", country=country)
-print(df.shape)
-for grp_name, grp in df.groupby('site_name'):
-    plot(grp['obs_value'], '--.', label=grp_name, show=False,
-         ax_kws=dict(figsize=(18,7)))
-plt.show()
-
-# %%
-df = ds.fetch_parameter("TSS", country=country)
-print(df.shape)
-for grp_name, grp in df.groupby('site_name'):
-    plot(grp['obs_value'], '--.', label=grp_name, show=False,
-         ax_kws=dict(figsize=(18,7)))
-plt.show()
-
-# %%
-df = ds.fetch_parameter("TP", country=country)
-print(df.shape)
-for grp_name, grp in df.groupby('site_name'):
-    plot(grp['obs_value'], '--.', label=grp_name, show=False,
-         ax_kws=dict(figsize=(18,7)))
-plt.show()
-
-# %%
-df = ds.fetch_parameter("pH", country=country)
-print(df.shape)
-for grp_name, grp in df.groupby('site_name'):
-    plot(grp['obs_value'], '--.', label=grp_name, show=False,
-         ax_kws=dict(figsize=(18,7)))
-plt.show()
+# df = ds.fetch_parameter("TEMP", country=country)
+# print(df.shape)
+# for grp_name, grp in df.groupby('site_name'):
+#     plot(grp['obs_value'], '--.', label=grp_name, show=False,
+#          ax_kws=dict(figsize=(18,7)))
+# plt.show()
+#
+# # %%
+# df = ds.fetch_parameter("NH4N", country=country)
+# print(df.shape)
+# for grp_name, grp in df.groupby('site_name'):
+#     plot(grp['obs_value'], '--.', label=grp_name, show=False,
+#          ax_kws=dict(figsize=(18,7)))
+# plt.show()
+#
+# # %%
+# df = ds.fetch_parameter("DO", country=country)
+# print(df.shape)
+# for grp_name, grp in df.groupby('site_name'):
+#     plot(grp['obs_value'], '--.', label=grp_name, show=False,
+#          ax_kws=dict(figsize=(18,7)))
+# plt.show()
+#
+# # %%
+# df = ds.fetch_parameter("COD", country=country)
+# print(df.shape)
+# for grp_name, grp in df.groupby('site_name'):
+#     plot(grp['obs_value'], '--.', label=grp_name, show=False,
+#          ax_kws=dict(figsize=(18,7)))
+# plt.show()
+#
+# # %%
+# df = ds.fetch_parameter("BOD", country=country)
+# print(df.shape)
+# for grp_name, grp in df.groupby('site_name'):
+#     plot(grp['obs_value'], '--.', label=grp_name, show=False,
+#          ax_kws=dict(figsize=(18,7)))
+# plt.show()
+#
+# # %%
+# df = ds.fetch_parameter("DON", country=country)
+# print(df.shape)
+# for grp_name, grp in df.groupby('site_name'):
+#     plot(grp['obs_value'], '--.', label=grp_name, show=False,
+#          ax_kws=dict(figsize=(18,7)))
+# plt.show()
+#
+# # %%
+# df = ds.fetch_parameter("DOSAT", country=country)
+# print(df.shape)
+# for grp_name, grp in df.groupby('site_name'):
+#     plot(grp['obs_value'], '--.', label=grp_name, show=False,
+#          ax_kws=dict(figsize=(18,7)))
+# plt.show()
+#
+# # %%
+# df = ds.fetch_parameter("TDP", country=country)
+# print(df.shape)
+# for grp_name, grp in df.groupby('site_name'):
+#     plot(grp['obs_value'], '--.', label=grp_name, show=False,
+#          ax_kws=dict(figsize=(18,7)))
+# plt.show()
+#
+# # %%
+# df = ds.fetch_parameter("TKN", country=country)
+# print(df.shape)
+# for grp_name, grp in df.groupby('site_name'):
+#     plot(grp['obs_value'], '--.', label=grp_name, show=False,
+#          ax_kws=dict(figsize=(18,7)))
+# plt.show()
+#
+# # %%
+# df = ds.fetch_parameter("TSS", country=country)
+# print(df.shape)
+# for grp_name, grp in df.groupby('site_name'):
+#     plot(grp['obs_value'], '--.', label=grp_name, show=False,
+#          ax_kws=dict(figsize=(18,7)))
+# plt.show()
+#
+# # %%
+# df = ds.fetch_parameter("TP", country=country)
+# print(df.shape)
+# for grp_name, grp in df.groupby('site_name'):
+#     plot(grp['obs_value'], '--.', label=grp_name, show=False,
+#          ax_kws=dict(figsize=(18,7)))
+# plt.show()
+#
+# # %%
+# df = ds.fetch_parameter("pH", country=country)
+# print(df.shape)
+# for grp_name, grp in df.groupby('site_name'):
+#     plot(grp['obs_value'], '--.', label=grp_name, show=False,
+#          ax_kws=dict(figsize=(18,7)))
+# plt.show()
 
 # %%
