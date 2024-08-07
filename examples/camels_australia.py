f6b73c24778de2ea8e82ff0c73b7d1161e220037
AtrCheema <ather_abbas786@yahoo.com>
2024-08-07 13:52:56 +0000

    updated example

--- examples/camels_australia.py
+++ examples/camels_australia.py
@@ -7,11 +7,15 @@ import os
 import site
 
 if __name__ == '__main__':
-    wd_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath('__file__')))))
-    # wd_dir = os.path.dirname(os.path.dirname(os.path.realpath('__file__')))
+    # wd_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath('__file__')))))
+    wd_dir = os.path.dirname(os.path.dirname(os.path.realpath('__file__')))
     print(wd_dir)
     site.addsitedir(wd_dir)
 
+from ai4water.eda import EDA
+import matplotlib.pyplot as plt
+from easy_mpl import scatter, hist
+from easy_mpl.utils import process_cbar
 from water_datasets import CAMELS_AUS
 from water_datasets.utils import print_info
 
@@ -24,10 +28,213 @@ print_info()
 dataset = CAMELS_AUS()
 
 # %%
+dataset.start
 
+# %%
+dataset.end
+
+# %%
+
+stations = dataset.stations()
+len(stations)
+
+# %%
+
+stations[0:10]
+
+# %%
+# Static Features
+# ---------------
+
+dataset.static_features
+
+# %%
+
+len(dataset.static_features)
+
+# %%
+
+mrvbf = 'proportion of catchment occupied by classes of MultiResolution Valley Bottom Flatness'
+lc01 = 'land cover codes'
+nvis = 'vegetation sub-groups'
+anngro = 'Average annual growth index value for some plants'
+gromega = 'Seasonality of growth index value'
+npp = 'net primary productivity'
+
+
+# %%
+
+static = dataset.fetch_static_features(stn_id=stations)
+static.shape
+
+# %%
+
+EDA(data=static, save=False).heatmap()
+
+# %%
+
+physical_features = []
+soil_features = []
+geological_features = []
+flow_characteristics = []
+
+static = static.dropna(axis=1)
+static.shape
+
+# %%
+coords = dataset.stn_coords()
+coords
+
+# %%
+
+dataset.plot_stations()
+
+# %%
+
+lat = coords['lat'].astype(float).values.reshape(-1,)
+long = coords['long'].astype(float).values.reshape(-1,)
+
+# %%
+
+idx = 0
+ax_num = 0
+
+fig, axes = plt.subplots(5, 5, figsize=(15, 12))
+axes = axes.flatten()
+
+while ax_num < 25:
+
+    val = static.iloc[:, idx]
+    idx += 1
+
+    try:
+        c = val.astype(float).values.reshape(-1,)
+
+        en = 222
+        ax = axes[ax_num]
+        ax, sc = scatter(long[0:en], lat[0:en], c=c[0:en], cmap="hot", show=False, ax=ax)
+
+        process_cbar(ax, sc, border=False, title=val.name, #title_kws ={"fontsize": 14}
+                    )
+        ax_num += 1
+    except ValueError:
+        continue
+
+
+
+plt.tight_layout()
+plt.show()
+print(idx)
+
+# %%
+
+idx = 32
+ax_num = 0
+
+fig, axes = plt.subplots(5, 5, figsize=(15, 12))
+axes = axes.flatten()
+
+while ax_num < 25:
+
+    val = static.iloc[:, idx]
+    idx += 1
+
+    try:
+        c = val.astype(float).values.reshape(-1,)
+
+        en = 222
+        ax = axes[ax_num]
+        ax, sc = scatter(long[0:en], lat[0:en], c=c[0:en], cmap="hot", show=False, ax=ax)
+
+        process_cbar(ax, sc, border=False, title=val.name, #title_kws ={"fontsize": 14}
+                    )
+        ax_num += 1
+    except ValueError:
+        continue
+
+
+
+plt.tight_layout()
+plt.show()
+print(idx)
+
+# %%
+
+idx = 59
+ax_num = 0
+
+fig, axes = plt.subplots(5, 5, figsize=(15, 12))
+axes = axes.flatten()
+
+while ax_num < 25:
+
+    val = static.iloc[:, idx]
+    idx += 1
+
+    try:
+        c = val.astype(float).values.reshape(-1,)
+
+        en = 222
+        ax = axes[ax_num]
+        ax, sc = scatter(long[0:en], lat[0:en], c=c[0:en], cmap="hot", show=False, ax=ax)
+
+        process_cbar(ax, sc, border=False, title=val.name, #title_kws ={"fontsize": 14}
+                    )
+        ax_num += 1
+    except ValueError:
+        continue
+
+
+
+plt.tight_layout()
+plt.show()
+print(idx)
+
+
+# %%
+# Dyanmic Features
+# ==================
+dataset.dynamic_features
+
+# %%
+# Streamflow
+# -----------
+streamflow = dataset.q_mmd()
+
+streamflow.shape
+
+# %%
+streamflow
+
+# %%
+
+EDA(data=streamflow, save=False).heatmap()
+
+# %%
+
+fig, axes = plt.subplots(7, 7, figsize=(10, 10), sharey="all")
+
+for idx, ax in enumerate(axes.flat):
+
+    hist(streamflow.iloc[:, idx].values.reshape(-1,),
+         bins=20,
+         ax=ax,
+         show=False
+        )
+
+plt.show()
+
+# %%
+
+_ = hist(streamflow.skew().values.reshape(-1,), bins=50)
+
+# %%
 df = dataset.fetch(stations=1, as_dataframe=True)
 df = df.unstack() # the returned dataframe is a multi-indexed dataframe so we have to unstack it
 df.shape
+
+# %%
+df
 
 # %%
 
@@ -45,13 +252,11 @@ df.shape
 df
 
 # %%
-
 # The returned dataframe is a multi-indexed data
 df.index.names == ['time', 'dynamic_features']
 
 df
 # %%
-
 # get data by station id
 df = dataset.fetch(stations='224214A', as_dataframe=True).unstack()
 df.shape
@@ -61,7 +266,6 @@ df.shape
 df
 
 # %%
-
 # get names of available dynamic features
 dataset.dynamic_features
 # get only selected dynamic features
